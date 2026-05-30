9ec1d9871b39101b72458a460a5426a921e7f0c6
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2026-05-30 01:21:56 -0600

    fix:modificamos el constructor  de nuestra clase para trabajar con el login. primero injectamos  el HttpClient dentro del constructor de la clase AuthService.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+/**.  1.-traemos un short para ocupar los ambientes y de esa manera trabajaos con las llamadas a httpclient*/
+import { environment } from '@environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) 
+  { }
 
 
   login(email: string, password: string){
